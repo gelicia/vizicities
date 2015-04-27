c9d96f7a8d13329e379c9e0d0e9865cc9d240a76
Kristina <kdurivage@gelicia.com>
2015-04-26 19:46:30 -0500

    Add new color scale!

--- src/Blueprint/BlueprintOutputDebugPoints-GPS.js
+++ src/Blueprint/BlueprintOutputDebugPoints-GPS.js
@@ -49,7 +49,20 @@
   VIZI.BlueprintOutputDebugPointsGPS.prototype.outputPoints = function(data) {
     var self = this;
 
-   // console.log(data);
+    //gets an array of unique wireless network names
+    var colorMap = _.uniq(data, false, function(d){
+      return d.values[0];
+    }).map(function(d){ return d.values[0];});
+
+    console.log(colorMap);
+
+    var colorScale = d3.scale.cubehelix()
+        .domain([0, (colorMap.length/2), colorMap.length])
+        .range([
+          d3.hsl(-100, 0.75, 0.35),
+          d3.hsl(  80, 1.50, 0.80),
+          d3.hsl( 260, 0.75, 0.35)
+        ]);
 
     var nestedData = d3.nest()
     .key(function(d) { return d.coordinates[0] + "," + d.coordinates[1]; })
@@ -69,46 +82,51 @@
     _.each(nestedData, function(point) {
       //the data is nested on coordinates so we can just use the first value
       var coords = point.values[0].coordinates;
-      var colors = d3.scale.category20();
       var heightOffset = 0;
 
-      for (var i = 0; i < point.values.length; i++) {
-        var material = new THREE.MeshBasicMaterial({
-          color: colors(i),
-          // vertexColors: THREE.VertexColors,
-          // ambient: 0xffffff,
-          // emissive: 0xcccccc,
-          shading: THREE.FlatShading
+      for (var i = 0; i < (point.values.length); i++){//point.values.length; i++) {
+
+       getIndex(colorMap, point.values[i].values[0]).then(function(colorIndex){
+          
+          //console.log("what", point.values[i].values[0], colorIndex);
+          
+          var material = new THREE.MeshBasicMaterial({
+            color: colorScale(colorIndex),
+            // vertexColors: THREE.VertexColors,
+            // ambient: 0xffffff,
+            // emissive: 0xcccccc,
+            shading: THREE.FlatShading
+          });
+
+          var offset = new VIZI.Point();
+          var geoCoord = self.world.project(new VIZI.LatLon(coords[1], coords[0]));
+
+          offset.x = -1 * geoCoord.x;
+          offset.y = -1 * geoCoord.y;
+
+          //get height from strength
+          var height = point.values[i].values[2]/4;
+
+          //console.log("height", height);
+
+          var mesh = new THREE.Mesh(barGeom, material);
+
+          mesh.scale.y = height;
+
+          // Offset
+          mesh.position.x = -1 * offset.x;
+          mesh.position.z = -1 * offset.y;
+          mesh.position.y = heightOffset;
+
+          heightOffset += height;
+
+          // Flip as they are up-side down
+          // mesh.rotation.x = 90 * Math.PI / 180;
+
+          mesh.matrixAutoUpdate && mesh.updateMatrix();
+          self.add(mesh);
         });
-
-        var offset = new VIZI.Point();
-        var geoCoord = self.world.project(new VIZI.LatLon(coords[1], coords[0]));
-
-        offset.x = -1 * geoCoord.x;
-        offset.y = -1 * geoCoord.y;
-
-        //get height from strength
-        var height = point.values[i].values[2]/4;
-
-        console.log("height", height);
-
-        var mesh = new THREE.Mesh(barGeom, material);
-
-        mesh.scale.y = height;
-
-        // Offset
-        mesh.position.x = -1 * offset.x;
-        mesh.position.z = -1 * offset.y;
-        mesh.position.y = heightOffset;
-
-        heightOffset += height;
-
-        // Flip as they are up-side down
-        // mesh.rotation.x = 90 * Math.PI / 180;
-
-        mesh.matrixAutoUpdate && mesh.updateMatrix();
-        self.add(mesh);
-      };
+      }
     });
   };
 
@@ -117,4 +135,16 @@
     self.world = world;
     self.init();
   };
+
+  function getIndex(array, name){
+    var deferred = Q.defer();
+
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] === name){
+        deferred.resolve(i);
+      }
+    }
+
+    return deferred.promise;
+  }
 }());
